8b7f2c7d87a92d030a83c508b1b8fc04fc3a9236
AdamUArk <jahughes@uark.edu>
2020-03-21 00:09:03 -0500

    fit

--- SCRIPT.R
+++ SCRIPT.R
@@ -5,7 +5,9 @@ train <- read_csv("covid19-local-us-ca-forecasting-week-1/ca_train.csv")
 summary(train)
 
 train %>%
-  arrange(desc(Date)) %>%
-  ggplot(aes(x = Date, y = ConfirmedCases)) +
+  arrange(desc(Lat)) %>%
+  ggplot(aes(x = Date, y = ConfirmedCases, color = Fatalities)) +
     geom_point() +
-    geom_smooth(lm = "smooth")
+    geom_smooth(span = 0.3)
+    # geom_smooth(method = lm, se = FALSE)
+    # geom_smooth(method = lm, formula = y ~ splines::bs(x, 3), se = FALSE)
